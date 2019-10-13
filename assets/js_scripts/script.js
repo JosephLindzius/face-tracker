1949e4e9d6e9245f8c227e70c795631cfcfdb8d5
Joseph Lindzius <46001761+Blauwmaan@users.noreply.github.com>
2019-10-13 15:10:58 +0200

    try again with path

--- assets/js_scripts/script.js
+++ assets/js_scripts/script.js
@@ -3,10 +3,10 @@
 const webcamVideo = document.getElementById('webcamVideo');
 //STEP THREE
 Promise.all([
-   faceapi.nets.tinyFaceDetector.loadFromUri("../models"),
-    faceapi.nets.faceLandmark68Net.loadFromUri("../models"),
-    faceapi.nets.faceRecognitionNet.loadFromUri("../models"),
-    faceapi.nets.faceExpressionNet.loadFromUri("../models")
+    faceapi.nets.tinyFaceDetector.loadFromUri("../../assets/models"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("../../assets/models"),
+    faceapi.nets.faceRecognitionNet.loadFromUri("../../assets/models"),
+    faceapi.nets.faceExpressionNet.loadFromUri("../../assets/models")
 ]).then(getVideo);
 
 
@@ -23,7 +23,10 @@ function getVideo () {
 //STEP TWO
 webcamVideo.addEventListener('play', function(){
     alert("I am playing!");
-
+    //STEP FIVE
+    const canvas = faceapi.createCanvasFromMedia(webcamVideo);
+    document.body.append(canvas);
+    const DISPLAY_SIZE = { width: webcamVideo.width, height: webcamVideo.height };
     //STEP FOUR
     setInterval(async function () {
         const detections = await faceapi.detectAllFaces(webcamVideo,
