81f0e26f060e76d106e131e11a16675f4c7850ab
Joseph Lindzius <46001761+Blauwmaan@users.noreply.github.com>
2019-10-13 14:21:01 +0200

    now in promises webcam

--- assets/js_scripts/script.js
+++ assets/js_scripts/script.js
@@ -1,11 +1,14 @@
 const webcamVideo = document.getElementById('webcamVideo');
 
+
 function getVideo () {
-    navigator.getUserMedia(
-        { video: {} },
-        stream => webcamVideo.srcObject = stream,
-        err => console.error(err)
-    )
+    navigator.mediaDevices.getUserMedia({ video: {} })
+        .then(function(stream) {
+            webcamVideo.srcObject = stream;
+        })
+        .catch(function(err) {
+            console.error(err);
+        });
 }
 
 getVideo();
