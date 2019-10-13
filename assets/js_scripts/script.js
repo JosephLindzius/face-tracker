8a3180a1c1db8ea4cb63d3364c604ef9200b82aa
Joseph Lindzius <46001761+Blauwmaan@users.noreply.github.com>
2019-10-13 15:00:23 +0200

    added script faceapi

--- assets/js_scripts/script.js
+++ assets/js_scripts/script.js
@@ -1,12 +1,16 @@
-const webcamVideo = document.getElementById('webcamVideo');
+//STEP ZERO
 
+const webcamVideo = document.getElementById('webcamVideo');
+//STEP THREE
 Promise.all([
    faceapi.nets.tinyFaceDetector.loadFromUri("../models"),
-    faceapi.nets.faceLandmark68Net.loadFromUri("../model"),
+    faceapi.nets.faceLandmark68Net.loadFromUri("../models"),
     faceapi.nets.faceRecognitionNet.loadFromUri("../models"),
     faceapi.nets.faceExpressionNet.loadFromUri("../models")
 ]).then(getVideo);
 
+
+//STEP ONE
 function getVideo () {
     navigator.mediaDevices.getUserMedia({ video: {} })
         .then(function(stream) {
@@ -16,9 +20,11 @@ function getVideo () {
             console.error("err");
         });
 }
-
+//STEP TWO
 webcamVideo.addEventListener('play', function(){
     alert("I am playing!");
+
+    //STEP FOUR
     setInterval(async function () {
         const detections = await faceapi.detectAllFaces(webcamVideo,
             new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions();
